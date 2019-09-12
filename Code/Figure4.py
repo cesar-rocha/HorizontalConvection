69964750273912fd0f5d3f2393a0a867595a7147
cesar-rocha <rocha.cesarb@protonmail.com>
2019-09-12 16:22:24 -0400

    Updates figure4

--- Code/Figure4.py
+++ Code/Figure4.py
@@ -3,55 +3,84 @@
     definition of the Nusselt number and scaling second paper,'
     by C.B. Rocha, T. Bossy, N.C. Constantinou, S.G. Llewellyn Smith
     & W.R. Young, submitted to JFM.
-
-    Figure4.py: time series of Nusselt number for 2D and 3D
-                free-slip and no-slip solutions with Ra=6.4e9,
-                with a "cold start."
-
+    Figure4.py: time series of kinetic energy, Nusselt number, and
+                bottom buoyancy for 2D solutions with Ra=6.4e9.
     Cesar Rocha et al.
     WHOI, Spring 2019
-
 """
+
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-plt.close("all")
+import h5py
 
 # Load data
-data2d_nostress = np.load('../Data/NuAndKE_2D_nostress_6p4e10.npz')
-data2d_noslip = np.load('NuAndKE_2D_noslip_6p4e10.npz')
-data3d_nostress = np.load('NuAndKE_3D_nostress_6p4e10.npz')
-data3d_noslip = np.load('NuAndKE_3D_noslip_6p4e10.npz')
+Nu_ns = np.load("../Data/NuAndKE_nostress.npz")
+Nu_sl = np.load("../Data/NuAndKE_noslip.npz")
+Bb_ns = np.load("../Data/BottomBuoyancy_nostress.npz")
+Bb_sl = np.load("../Data/BottomBuoyancy_noslip.npz")
+
+# Normalization factor (diffusive time scale)
+Tkappa = 1e4
 
 # Plotting
-fig = plt.figure(figsize=(10,6))
-ax = fig.add_subplot(111)
+fig = plt.figure(figsize=(8.5,10.5))
 
-p1 = ax.plot(data3d_nostress['time'],data3d_nostress['Nu'],'b')
-ax.plot(data2d_nostress['time'],data2d_nostress['Nu'],'--',color=p1[0].get_color(),linewidth=.5)
+ax1 = fig.add_subplot(311)
 
-ax.plot(data3d_noslip['time'],data3d_noslip['Nu'],'r')
-p2 = ax.plot(data2d_noslip['time'],data2d_noslip['Nu'],'r--',linewidth=.5)
+plt.semilogy(Nu_ns['time']/Tkappa,Nu_ns['ke'],'b')
+plt.semilogy(Nu_sl['time']/Tkappa,Nu_sl['ke'],'r')
 
-plt.xlim(-0.005,0.125)
-plt.ylim(0,100)
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
+plt.xlim([-0.005,.6])
+plt.ylim([5e-4,.045])
 
-plt.text(.07,45.5,'free-slip',color='b')
-plt.text(.11,22.5,'no-slip',color='r')
+plt.ylabel(r'$\langle u^2 + w^2 \rangle/2 h b_*$')
+plt.text(0,0.0455,'(a)')
 
-plt.xlabel(r'$\kappa t/h^2 $')
+plt.xlabel(r'$\kappa\, t /h^2$')
+
+ax1.spines['right'].set_visible(False)
+ax1.spines['top'].set_visible(False)
+
+ax2 = fig.add_subplot(312)
+
+plt.plot(Nu_ns['time']/Tkappa,Nu_ns['Nu'],'b')
+plt.plot(Nu_sl['time']/Tkappa,Nu_sl['Nu'],'r')
+
+plt.text(.45,26,'free-slip',color='b')
+plt.text(.4,11.,'no-slip',color='r')
+
+plt.xlim([-0.005,.6])
+plt.ylim([0,55])
+
+plt.xlabel(r'$\kappa\, t /h^2$')
+
+ax2.spines['right'].set_visible(False)
+ax2.spines['top'].set_visible(False)
+
 plt.ylabel(r'Nu')
+plt.text(0,56,'(b)')
 
-plt.text(.125,44.9,'3D')
-plt.text(.125,35.5,'2D')
-plt.text(.125,30.75,'3D')
-plt.text(.125,26.7,'2D')
+plt.xlabel(r'$\kappa\, t /h^2$')
 
-plt.savefig("Figure5.png", bbox_inches = 'tight',
-            pad_inches = 0)
+ax3 = fig.add_subplot(313)
 
-plt.savefig("Figure5.eps", bbox_inches = 'tight',
-            pad_inches = 0)
+plt.plot(Bb_ns['time']/Tkappa,Bb_ns['b'],'b')
+plt.plot(Bb_sl['time']/Tkappa,Bb_sl['b'],'r')
+
+plt.xlim([-0.005,.6])
+
+ax3.spines['right'].set_visible(False)
+ax3.spines['top'].set_visible(False)
+
+plt.ylabel(r'$\bar{b}(0)/b_*$')
+plt.xlabel(r'$\kappa\, t /h^2$')
+plt.text(0,0.045,'(c)')
+
+plt.yticks([0,-.25,-.5,-.75])
+
+plt.savefig("../Figz/Figure4.png",dpi=400, bbox_inches = 'tight',
+    pad_inches = 0)
+plt.savefig("../Figz/Figure4.eps", bbox_inches = 'tight',
+    pad_inches = 0)
+
